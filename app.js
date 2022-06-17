68c4d6e28056b4669e55a4c6535581a2b155a587
DimaSurdiy <dsurdiy@ukr.net>
2022-06-17 06:41:05 +0300

    Add express handlebars

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const exhbs = require('express-handlebars');
 
 const app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'hbs');
+app.engine(
+  'hbs',
+  exhbs({
+    extname: 'hbs',
+  }),
+);
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1><h2>Это страничка /</h2>');
