3cf8d9dbbb20a9d37fb920c39760202ed61f36b4
DimaSurdiy <dsurdiy@ukr.net>
2022-06-17 06:07:51 +0300

    Add basic app routing

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1><h2>Это страничка /</h2>');
+
+  console.log(req.url);
+});
+
+app.get('/about', (req, res) => {
+  res.send('<h2>Это страничка About</h2>');
+
+  console.log(req.url);
+});
+
 app.listen(4444, () => {
   console.log(`Application server is running on port ${4444}`);
 });
