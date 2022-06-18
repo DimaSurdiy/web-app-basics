28897c5cfebd1516bf95b113083d8e3449cb5c8e
DimaSurdiy <dsurdiy@ukr.net>
2022-06-18 07:37:04 +0300

    Add dynamic product pages

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const exhbs = require('express-handlebars');
 const products = require('./products.json');
-
 const app = express();
 
 app.use(express.static('public'));
-
 app.engine(
   'hbs',
   exhbs.engine({
@@ -28,6 +26,12 @@ app.get('/products', (req, res) => {
     cssFileName: 'products',
     pageTitle: 'Наши товары',
   });
+});
+
+app.get('/product/:productId', (req, res) => {
+  const product = products.find(p => p.id === req.params.productId);
+
+  res.render('product', { product, productPageTitle: product.name });
 });
 
 app.listen(4444, () => {
